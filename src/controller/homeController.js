3df355c47008497ad3888658e29742d2b366bfeb
Hieu <gbmhieu123@gmail.com>
2022-09-18 23:19:21 +0700

    update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,5 @@
 import { json } from 'body-parser';
-import e from 'express';
+import express from 'express';
 import pool from '../configs/connectDB.js';
 
 let getHomePage = async (req, res) => {
@@ -33,8 +33,30 @@ let createNewUser = async (req, res) => {
   await pool.execute('INSERT INTO users(firstName, lastName, email, address) VALUES (?,?,?,?)', [firstName, lastName, email, address]);
   return res.redirect('/');
 }
+
+let deleteUser = async (req, res) =>{
+  await pool.execute(`DELETE FROM users WHERE userId = ${req.body.userId}`);
+  return res.redirect('/');
+}
+
+let editUser = async (req, res) => {
+  let id = req.params.id;
+  let [user, fields] = await pool.execute(`SELECT * FROM users WHERE userId = ${id}`);
+  return res.render('updateUser.ejs', {dataUser: user[0]});
+}
+
+let updateUser = async (req, res) =>{
+    let {userId, firstName, lastName, email, address} = req.body;
+    console.log(userId, firstName, lastName, email, address);
+    await pool.execute(`UPDATE users SET firstName = ?, lastName = ?, email= ?, address = ? WHERE userId = ? `,
+    [firstName, lastName, email, address, userId]);
+    return res.redirect('/');
+}
 module.exports = {
     getHomePage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    editUser,
+    updateUser
 }
